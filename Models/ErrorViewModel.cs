cb3cc7e4bc1007d526c24bfaba3d25f3c477f268
ibrahim inan <ibrahiminan9955@gmail.com>
2023-08-10 01:47:30 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mywebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# mywebsite-mvc
+ my cv
